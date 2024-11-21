e18cfa39551164a688c28c80a031155cce6484d1
Casper2411 <ccaassppeerr2411@gmail.com>
2024-11-21 15:55:01 +0100

    fixed 12.1 and added 12.2

---
+++ Assignment11/MicroC/ex1.c
@@ -0,0 +1,9 @@
+void main()
+{
+    if (31 < 22)
+    {
+    }
+    else
+        print 1212;
+    print 33;
+}
---
+++ Assignment11/MicroC/ex2.c
@@ -0,0 +1,6 @@
+void main()
+{
+    if (30 != 30)
+        print 33;
+    print 2323;
+}
